6acbfec7274b192204a38e33c043fedb79b60102
Steven Kühnl <stevnkuehnl@gmail.com>
2024-01-17 15:16:37 +0100

    Made First Python Challenges

---
+++ PythonEdabitChallenges/pythonProject/medium/LengthOfNumber/LengthOfNumber.py
@@ -0,0 +1,12 @@
+# Link to Challenge: https://edabit.com/challenge/2zKetgAJp4WRFXiDT
+def number_length(number = 0):
+    print(len(str(number)))
+
+
+number_length(10) #2
+number_length(5000) #4
+number_length(0) #1
+number_length(4039182) #7
+number_length(9999999999999999) #16
+number_length(1) #1
+number_length(777777777777777777777777777777) #30
