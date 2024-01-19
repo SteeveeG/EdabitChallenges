9f7535879480a18abcb84ac46c47a183acbd959e
Steven Kühnl <stevnkuehnl@gmail.com>
2024-01-19 13:18:52 +0100

    new Challenge Censored Stings

---
+++ PythonEdabitChallenges/pythonProject/Hard/CensoredStrings.py
@@ -0,0 +1,11 @@
+def uncensor(sentence="", vowels=""):
+    for v in vowels:
+        sentence = sentence.replace('*', v, 1)
+    print(sentence)
+
+
+uncensor('Wh*r* d*d my v*w*ls g*?', 'eeioeo')
+uncensor('abcd', '')
+uncensor('*PP*RC*S*', 'UEAE')
+uncensor('Ch**s*, Gr*mm*t -- ch**s*', 'eeeoieee')
+uncensor('*l*ph*nt', 'Eea')
