6c532cbfd2ca2850f4c6bad7cc1ec7601045417a
Steven Kühnl <stevnkuehnl@gmail.com>
2024-02-09 13:42:31 +0100

    Fixed bug in Challenge

--- PythonEdabitChallenges/pythonProject/Expert/ConnectingWords.py
+++ PythonEdabitChallenges/pythonProject/Expert/ConnectingWords.py
@@ -1,20 +1,32 @@
 # link to Challenge : https://edabit.com/challenge/qNQkYzY8GpiFMmndh
 
+
 def join(lst):
-    no_join_count = 0
-    minimum_shared_letters = lst[0].__len__()
+    count = 0
     joined_word = ""
-    for i in range(lst.__len__() - 1):
-        word_count = lst[i].__len__()
-        for ii in range(word_count):
-            if lst[i][ii:word_count] in lst[i + 1]:
-                joined_word += lst[i].replace(lst[i][ii: word_count], "")
-                if minimum_shared_letters > lst[i].__len__() - ii: minimum_shared_letters = lst[i].__len__() - ii
-                break
-            elif ii + 1 == lst[i].__len__():
-                no_join_count += 1
-                joined_word += lst[i]
-    if no_join_count == lst.__len__() - 1: minimum_shared_letters = 0
+    minimum_shared_letters = sum(len(words) for words in lst)
+    for words in range(lst.__len__() - 1):
+        word_joined = False
+        word_count = lst[words].__len__()
+        next_word_count = lst[words + 1].__len__()
+        for pre_word in range(lst[words].__len__()):
+            preword = lst[words][pre_word:word_count]
+            for next_word in range(lst[words + 1].__len__()):
+                nexword1 = lst[words + 1][0:next_word_count - next_word]
+                if not word_joined:
+                    if preword == nexword1:
+                        joined_word += lst[words].replace(nexword1, "")
+                        if minimum_shared_letters > next_word_count - next_word:
+                            minimum_shared_letters = next_word_count - next_word
+                        word_joined = True
+                        break
+                    else:
+                        word_joined = False
+        if not word_joined:
+            joined_word += lst[words]
+            count += 1
+    if count == lst.__len__()-1:
+        minimum_shared_letters=0
     joined_word += lst[lst.__len__() - 1]
     joined = [joined_word, minimum_shared_letters]
     print(joined)
